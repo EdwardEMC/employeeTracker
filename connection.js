f1cf3a5c61f7a20db49d13283583d2a1a0b4c087
EMC <edwardcoad.ec@gmail.com>
2020-02-02 16:19:21 +1030

    main menu complete with switch case, main functions named

--- connection.js
+++ connection.js
@@ -14,9 +14,91 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    company();
+    mainMenu();
 });
 
-function company() {
-
+function mainMenu() {
+    connection.query("SELECT * FROM employee", function(err, result) {
+        if(err) throw err;
+        console.table(result);
+        mainInq();//inquirer to ask what the user wants to do
+    });
 };
+
+//function to view department, role or employee
+function vDeparment() {
+
+}
+
+//function to view employees by manager
+function vManagement() {
+
+}
+
+//function to view the budget of departments and business as whole
+function budget() {
+
+}
+
+//function to update employee role
+function upEmployee() {
+
+}
+
+//function to update employee managers
+function upManager() {
+
+}
+
+//function to add department, role, employee
+function aDepartment() {
+
+}
+
+//function to delete department, role or employee
+function dInfo() {
+
+}
+
+//========================================================================== 
+//Inquirer functions
+//==========================================================================
+//inquirer for mainMenu()
+function mainInq() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                choices: ["View Departments", "View employees by manger", "View budget", "Update an employee", "Update a manager", "Add department, role or employee", "Delete department, role or employee"],
+                name: "user_action"
+            }
+        ]).then(function(response) {
+            //switch case for options
+            switch(response.user_action) {
+                case "View Departments":
+                    vDeparment();
+                    break;
+                case "View employees by manger":
+                    vManagement();
+                    break;
+                case "View budget":
+                    budget();
+                    break;
+                case "Update an employee":
+                    upEmployee();
+                    break;
+                case "Update a manager":
+                    upManager();
+                    break;
+                case "Add department, role or employee":
+                    aDepartment();
+                    break;
+                case "Delete department, role or employee":
+                    dInfo();
+                    break;
+                default:
+                    mainMenu();
+            }
+        });
+} 
